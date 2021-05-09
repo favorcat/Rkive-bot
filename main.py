f863447683777f31b18931f1c4079e35163dc51d
favorcat <favorcat.me@gmail.com>
2021-05-09 23:52:57 +0900

    correct timeline error

--- main.py
+++ main.py
@@ -14,24 +14,69 @@ bot = telegram.Bot(token = telgm_token)
 All_account = ['BTS_twt', 'bts_bighit', 'BTS_jp_official', 'bts_love_myself', 'Smeraldo_Books', 'INTHESOOP_TV', 'BT21_', 'BT21_Japan', 'TinyTANofficial', 'BTSW_official']
 select_account = ['BIGHIT_MUSIC', 'weverseofficial', 'HYBEOFFICIALtwt', 'HYBE_LABELS_JP', 'weverseshop', 'HYBE_MERCH', 'BigHitShop', 'RhythmHive_twt', 'fila_korea', 'Coway_Global']
 
-def find_new_tweet(account):
-  with open('./media/'+account+'.json', 'r') as save_file:
-    json_data = json.load(save_file)
-    
-  tweet = api.GetUserTimeline(screen_name=account, count=1, include_rts=True, exclude_replies=False)
+# 로그
+print(datetime.today().strftime("%Y.%m.%d %H:%M:%S"))
 
-  if (tweet[0].text != json_data['text']):
-    save_tweet = html.unescape(tweet[0].text)
-    save_url = 'https://twitter.com/'+str(tweet[0].user.screen_name)+'/status/'+str(tweet[0].id)
-    sendTxt = str(tweet[0].user.name)+'님이 새로운 트윗을 올렸습니다!'+'\n\n'+save_tweet+'\n\n'+save_url
-    twtSendTxt = '@'+str(tweet[0].user.screen_name)+' 님의 새 트윗\n#BTS #BTS_Butter #Rkive'+'\n'+save_url
-    # 채널의 경우 chat_id = '- n'
-    bot.sendMessage(chat_id = '', text=sendTxt)
-    api.PostUpdate(twtSendTxt)
+def find_new_tweet(account):
+    tweet = api.GetUserTimeline(screen_name=account, count=5, include_rts=True, exclude_replies=False)
     
-    with open('./media/'+account+'.json', 'w', encoding="utf-8") as make_file:
-      for status in tweet:
-        print(status, file=make_file)
+    with open('./media/'+account+'.json', 'r', encoding="utf-8") as save_file:
+        json_data = json.load(save_file)
+
+        save_index = 0
+        for i in range(4, -1, -1):
+            for k in range(5):
+                if (tweet[i].text == json_data[k]['text']):
+                    save_index = i
+
+        if (save_index != 0):
+            for i in range(save_index):
+                save_tweet = html.unescape(tweet[i].text)
+                save_url = 'https://twitter.com/'+str(tweet[i].user.screen_name)+'/status/'+str(tweet[i].id)
+                sendTxt = str(tweet[i].user.name)+'님이 새로운 트윗을 올렸습니다!'+'\n\n'+save_tweet+'\n\n'+save_url
+                twtSendTxt = str(tweet[i].user.screen_name)+' 님의 새 트윗\n#BTS #BTS_Butter #Rkive #JIN #석진 #SUGA #윤기 #RM #남준 #JHOPE #호석 #JIMIN #지민 #V #태형 #정국 #JK'+'\n'+save_url
+
+                # 채널의 경우 chat_id = '- n'
+                bot.sendMessage(chat_id = '', text=sendTxt)
+                api.PostUpdate(twtSendTxt)
+
+                print(str(tweet[0].user.name)+'님이 새로운 트윗을 올렸습니다!')
+
+            with open('./media/'+account+'.json', 'w', encoding="utf-8") as make_file:
+                json.dump(list(map(lambda x: x.AsDict(), tweet)), make_file)
+
+def find_BTS_new_tweet(account):
+    tweet = api.GetUserTimeline(screen_name=account, count=10, include_rts=True, exclude_replies=False)
+    ans = []
+    for status in tweet:
+        if (any(status.text.find(x) != -1 for x in ["방탄소년단", "BTS", "TinyTAN"])) :
+            ans.append(status.AsDict())
+        if len(ans) == 5: break
+
+    with open('./media/'+account+'.json', 'r', encoding="utf-8") as save_file:
+        json_data = json.load(save_file)
+
+        save_index = 0
+        for i in range(4, -1, -1):
+            for k in range(5):
+                if (ans[i]['text'] == json_data[k]['text']):
+                    save_index = i
+
+        if (save_index != 0):
+            for i in range(save_index):
+                save_tweet = html.unescape(ans[i]['text'])
+                save_url = 'https://twitter.com/'+str(ans[i]['user']['screen_name'])+'/status/'+str(ans[i]['id'])
+                sendTxt = str(ans[i]['user']['name'])+'님이 새로운 트윗을 올렸습니다!'+'\n\n'+save_tweet+'\n\n'+save_url
+                twtSendTxt = str(ans[i]['user']['screen_name'])+' 님의 새 트윗\n#BTS #BTS_Butter #Rkive #JIN #석진 #SUGA #윤기 #RM #남준 #JHOPE #호석 #JIMIN #지민 #V #태형 #정국 #JK'+'\n'+save_url
+
+                # 채널의 경우 chat_id = '- n'
+                bot.sendMessage(chat_id = '', text=sendTxt)
+                api.PostUpdate(twtSendTxt)
+
+                print(str(tweet[0].user.name)+'님이 새로운 트윗을 올렸습니다!')
+
+            with open('./media/'+account+'.json', 'w', encoding="utf-8") as make_file:
+                json.dump(ans, make_file)
 
 # 계정 타임라인 전부 스트리밍
 for i in range(len(All_account)):
@@ -39,6 +84,4 @@ for i in range(len(All_account)):
 
 # 일부 단어 포함 스트리밍
 for k in range(len(select_account)):
-  tweet = api.GetUserTimeline(screen_name=select_account[k], count=100, include_rts=True, exclude_replies=False)
-  if ((tweet[0].text.find("BTS") != -1) or (tweet[0].text.find("방탄소년단") != -1)):
-    find_new_tweet(select_account[k])
+    find_BTS_new_tweet(select_account[k])
